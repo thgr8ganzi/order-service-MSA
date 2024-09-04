d2c066e9465e13c0ae8afdd290a5d0c13579252f
jisoo <q33asq33as@gmail.com>
2024-09-04 23:33:39 +0900

    Feat: api-order-service-MSA

---
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,67 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.service.OrderService;
+import com.example.orderservice.vo.RequestOrder;
+import com.example.orderservice.vo.ResponseOrder;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/order-service")
+public class OrderController {
+    private final Environment env;
+    private final OrderService orderService;
+    private final KafkaProducer kafkaProducer;
+
+    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer) {
+        this.env = env;
+        this.orderService = orderService;
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in Order Service on PORT %s", env.getProperty("local.server.port"));
+    }
+
+    @PostMapping("/{userId}/orders")
+    public ResponseEntity<ResponseOrder> createOrder(
+            @RequestBody RequestOrder order,
+            @PathVariable String userId
+    ) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        OrderDto orderDto = mapper.map(order, OrderDto.class);
+        orderDto.setUserId(userId);
+        OrderDto createdOrder = orderService.createOrder(orderDto);
+
+        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+
+        kafkaProducer.send("example-catalog-topic", orderDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public ResponseEntity<List<ResponseOrder>> getOrders(@PathVariable String userId) {
+        Iterable<OrderEntity> orders = orderService.getOrdersByUserId(userId);
+
+        List<ResponseOrder> result = new ArrayList<>();
+        orders.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseOrder.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
