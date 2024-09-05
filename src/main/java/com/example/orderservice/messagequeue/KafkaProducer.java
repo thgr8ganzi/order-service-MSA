a04c1fc64a732a7c25a2ec056ee0f81af2310cf9
jisoo <q33asq33as@gmail.com>
2024-09-05 19:24:47 +0900

    Feat: add kafka

---
+++ src/main/java/com/example/orderservice/messagequeue/KafkaProducer.java
@@ -0,0 +1,33 @@
+package com.example.orderservice.messagequeue;
+
+import com.example.orderservice.dto.OrderDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public OrderDto send(String topic, OrderDto orderDto) {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+        try {
+            jsonString = mapper.writeValueAsString(orderDto);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        kafkaTemplate.send(topic, jsonString);
+        log.info("Kafka Producer sent data from the Order Service: -> {}", orderDto.toString());
+        return orderDto;
+    }
+}
